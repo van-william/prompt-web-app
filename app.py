4b61057ad0bab65c35ef8ffc197cfdb11e8699de
William <william.n.vanbuskirk@gmail.com>
2022-11-03 22:14:42 -0500

    updated prompt

--- app.py
+++ app.py
@@ -4,16 +4,16 @@ import openai
 from flask import Flask, redirect, render_template, request, url_for
 
 app = Flask(__name__)
-openai.api_key = ""
+openai.api_key = os.getenv("OPENAI_API_KEY")
 
 
 @app.route("/", methods=("GET", "POST"))
 def index():
     if request.method == "POST":
-        animal = request.form["animal"]
+        sandwich_toppings = request.form["sandwich_toppings"]
         response = openai.Completion.create(
             model="text-davinci-002",
-            prompt=generate_prompt(animal),
+            prompt=generate_prompt(sandwich_toppings),
             temperature=0.6,
         )
         return redirect(url_for("index", result=response.choices[0].text))
@@ -22,14 +22,14 @@ def index():
     return render_template("index.html", result=result)
 
 
-def generate_prompt(animal):
-    return """Suggest three names for an animal that is a superhero.
+def generate_prompt(sandwich_toppings):
+    return """Suggest three names for a custom sandwich order.
 
-Animal: Cat
-Names: Captain Sharpclaw, Agent Fluffball, The Incredible Feline
-Animal: Dog
-Names: Ruff the Protector, Wonder Canine, Sir Barks-a-Lot
-Animal: {}
-Names:""".format(
-        animal.capitalize()
+Sandwich Toppings: Genoa Salami, Capocollo,Provolone, Hot Peppers, onion, lettuce, tomato & easy mayo
+Sandwich Name: Spicy East Coast Italian
+Sandwich Toppings: Turkey, Provolone, Avocado, cucumber, lettuce, tomato & mayo
+Sandwich Name: Beach Club
+Sandwich Toppings: {}
+Sandwich Name:""".format(
+        sandwich_toppings.capitalize()
     )
